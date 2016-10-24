b7a7d01341145651891eb9beecbdff469e1ccd7d
ChristopherRotter <christopher.rotter@stud.hs-mannheim.de>
2016-10-24 11:58:38 +0200

    Added Constructors
    Added constructors to classes Untoter and Nachtelf

--- 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/Nachtelf.java
+++ 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/Nachtelf.java
@@ -3,19 +3,29 @@ package tpe.oo.constructor;
 /**
  * Nachtelf aus der Allianz.
  */
-public class Nachtelf /* extends Figur */ {
+public class Nachtelf extends Wesen {
 
     /** Standardmäßige Stärke der Fähigkeit Naturwiderstand. */
     public static final int STANDARD_NATURWIDERSTAND = 5;
-    
+
     /** Fähigkeit zum Widerstand gegen Naturmagie */
     private int naturwiderstand;
-    
-    // Konstruktoren fehlen. Bitte hier einfügen.
-    
+
+    public Nachtelf(String name)
+    {
+        super(name);
+        naturwiderstand = STANDARD_NATURWIDERSTAND;
+    }
+
+    public Nachtelf(String name, int naturwiderstand)
+    {
+        this(name);
+        this.naturwiderstand = naturwiderstand;
+    }
+
     /**
      * Stärke des Naturwiderstandes.
-     * 
+     *
      * @return the naturwiderstand
      */
     public int getNaturwiderstand() {
--- 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/Untoter.java
+++ 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/Untoter.java
@@ -3,19 +3,29 @@ package tpe.oo.constructor;
 /**
  * Untoter aus der Horde.
  */
-public class Untoter /* extends Figur */ {
+public class Untoter extends Wesen {
 
     /** Standardmäßige Stärke der Fähigkeit zur Unterwasseratmung. */
     public static final int STANDARD_UNTERWASSERATMUNG = 10;
-    
+
     /** Fähigkeit zur Atmung unter Wasser. */
     private int unterwasseratmung;
-    
-    // Konstruktoren fehlen. Bitte hier einfügen.
-    
+
+    public Untoter(String name)
+    {
+        super(name);
+        unterwasseratmung = STANDARD_UNTERWASSERATMUNG;
+    }
+
+    public Untoter(String name, int unterwasseratmung)
+    {
+        this(name);
+        this.unterwasseratmung = unterwasseratmung;
+    }
+
     /**
      * Fähigkeit zur Unterwasseratmnung.
-     * 
+     *
      * @return the Stärke der Fähigkeit.
      */
     public int getUnterwasseratmung() {
